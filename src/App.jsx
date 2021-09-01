9e8a3241cb540c8c4a95c6d49e57e38db3607906
snehal-iblinfotech <snehal.chaudhari@iblinfotech.com>
2021-09-01 14:45:16 +0530

    react re itial

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import React, { useState } from 'react'
 import "bootstrap";
 import './App.scss';
-//componetns
-import Header from './Components/Header/Header'
-import Subheader from './Components/Subheader/Subheader';
 
 
-//pages
-import Minimal from './pages/Minimal'
+
 function App() {
 
 
   return (
     <div className="App ">
-      <Header/>
-      <Subheader/>
-      <Minimal />
+    hello
      </div>
   )
 }
